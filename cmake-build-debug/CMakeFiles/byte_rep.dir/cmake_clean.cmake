63cff68a70e954743906d3a97ebc3200c8b4fd4f
Hamish MacDonald <elusivenode@pop-os.localdomain>
2020-12-21 09:11:05 +1000

    program looking at byte representations

---
+++ cmake-build-debug/CMakeFiles/byte_rep.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/byte_rep.dir/ch_1/byte_rep.c.o"
+  "byte_rep"
+  "byte_rep.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/byte_rep.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
