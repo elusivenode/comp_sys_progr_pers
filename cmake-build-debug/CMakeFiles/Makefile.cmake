0d7515da0d7b71ebfe236e075f1ef9435911a978
Hamish MacDonald <elusivenode@pop-os.localdomain>
2020-12-21 14:24:33 +1000

    problem 2.5

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -44,5 +44,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/pp_2_5.dir/DependInfo.cmake"
   "CMakeFiles/byte_rep.dir/DependInfo.cmake"
   )
---
+++ cmake-build-debug/CMakeFiles/pp_2_5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pp_2_5.dir/ch_1/pp_2_5.c.o"
+  "pp_2_5"
+  "pp_2_5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pp_2_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
