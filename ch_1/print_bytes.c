026d40158a0787869eacff66119e07c017b52b7a
elusivenode <elusivenode@gmail.com>
2025-11-30 17:34:51 +1000

    relative paths

--- ch_1/print_bytes.c
+++ ch_1/print_bytes.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <limits.h>
+#include <libgen.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-  FILE *file = fopen("hello.c", "r");
+int main(int argc, char **argv) {
+  const char *path_from_root = "ch_1/hello.c";
+  const char *path_from_bin = "../../ch_1/hello.c";
+
+  FILE *file = fopen(path_from_root, "r");
+  if (file == NULL && argc > 0) {
+    char exe_path[PATH_MAX];
+    if (realpath(argv[0], exe_path) != NULL) {
+      char *dir_buf = strdup(exe_path);
+      if (dir_buf != NULL) {
+        char *dir = dirname(dir_buf);
+        char full_path[PATH_MAX];
+        int written = snprintf(full_path, sizeof(full_path), "%s/%s", dir, path_from_bin);
+        if (written > 0 && written < (int) sizeof(full_path)) {
+          file = fopen(full_path, "r");
+        }
+        free(dir_buf);
+      }
+    }
+  }
+
   if (file == NULL) {
     perror("Failed to open file");
     return 1;
--- ch_1/print_bytes.zig
+++ ch_1/print_bytes.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const file: std.fs.File = try std.fs.cwd().openFile("hello.c", .{});
+    const gpa = std.heap.page_allocator;
+    const file: std.fs.File = try openHelloFile(gpa);
     defer file.close();
 
     var buffer: [1]u8 = undefined;
@@ -11,4 +12,25 @@ pub fn main() !void {
         const ch = buffer[0];
         std.debug.print("{c}: hex: {x} dec: {d}\n", .{ ch, ch, ch });
     }
+}
+
+fn openHelloFile(allocator: std.mem.Allocator) !std.fs.File {
+    const primary = "ch_1/hello.c";
+    const fallback_from_bin = "../../ch_1/hello.c";
+
+    if (std.fs.cwd().openFile(primary, .{})) |file| {
+        return file;
+    } else |err| switch (err) {
+        error.FileNotFound => {},
+        else => return err,
+    }
+
+    const exe_path = try std.fs.selfExePathAlloc(allocator);
+    defer allocator.free(exe_path);
+
+    const exe_dir = std.fs.path.dirname(exe_path) orelse ".";
+    const fallback_path = try std.fs.path.join(allocator, &.{ exe_dir, fallback_from_bin });
+    defer allocator.free(fallback_path);
+
+    return std.fs.cwd().openFile(fallback_path, .{});
 }
